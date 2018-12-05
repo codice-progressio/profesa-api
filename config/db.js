315550ec5ad9b595dc6c0c34ddeb2f0ebb0bb790
Angel <rafa.yael@gmail.com>
2018-12-05 10:49:23 -0600

    FIX CORS para desarrollo y produccion definidos.

--- config/db.js
+++ config/db.js
@@ -6,9 +6,16 @@ const PORT_PRODUCTION = 3000;
 const URI_PRODUCTION = 'mongodb://localhost:27017/carrduci';
 const URI_DEVELOPMENT = 'mongodb://localhost:27017/hospitalDB';
 
+const ACCESS_CONTROL_ALLOW_ORIGIN = {
+    DEVELOPMENT: 'http://192.168.1.30',
+    PRODUCTION: 'http://localhost:4200'
+}
+
+
 module.exports.enviroment = (env) => {
     const uri = env ? URI_PRODUCTION : URI_DEVELOPMENT;
     const port = env ? PORT_PRODUCTION : PORT_DEVELOPMENT;
+    const ACAO = evn ? ACCESS_CONTROL_ALLOW_ORIGIN.PRODUCTION : ACCESS_CONTROL_ALLOW_ORIGIN.DEVELOPMENT;
 
     const msj_dev_bienvenida = col.danger('***********| PRECAUCIÓN |**********') + 'Estas en modo desarrollo.';
     const msj_pro_bienvenida = col.success('PRODUCCIóN ') + 'Estas en modo producción.';
@@ -17,6 +24,7 @@ module.exports.enviroment = (env) => {
     const msj_bd_err = col.danger('BD OFFLINE') + `No se pudo conectar a la BD. ${uri}`;
 
     const msj_mongo = col.success('SERVER ONLINE') + `Express esta corriendo en el puerto: ${port}`;
+
 
     // Selecciona cambia entre base de datos. 
 
@@ -29,7 +37,8 @@ module.exports.enviroment = (env) => {
         // Este es lo mismo que env pero para hacerlo más leible. 
         esModoProduccion: env,
         port: port,
-        msj_mongoose_ok: msj_mongo
+        msj_mongoose_ok: msj_mongo,
+        ACCESS_CONTROL_ALLOW_ORIGIN: ACAO
     };
     ENVIROMENT = o;
     return o;

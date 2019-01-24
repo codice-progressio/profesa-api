785789eb3cf1f53a6d02312809670c74b2da7eb2
Angel <rafa.yael@gmail.com>
2019-01-24 06:59:33 -0600

    SOLO CAMBIOS TENTATIVOS CANCELADOS Y /O POSPUESTOS

--- models/maquina.js
+++ models/maquina.js
@@ -7,6 +7,74 @@ var NombreAnteriorMaquinaShchema = require('../models/nombreAnteriorMaquina');
 var Schema = mongoose.Schema;
 
 
+// var orden_DatosDeTrabajoEnMaquina = new Schema({
+
+//         // El id del folio para hacer referencia a el y la informacion
+//         // facilitandomos la vida para los reportes. 
+//         folio: {type: ObjectId, ref: 'Folio'},
+//         // El id de la orden de la cual se va a guardar el regitro. 
+//         orden: {type: ObjectId, ref: 'Folio.folioLineas.ordenes'},
+//         // El estatus de la orden. 
+//         // 1 - Trabajando
+//         // 2 - En espera.
+//         estatus: { type: number},
+//         // El registro de los cambio para esta orden.
+//         // Si no hay cambio esto debe estar en nulo.
+//         cambio: {
+//             // La hora a la que inicio el cambio. 
+//             inicio: { type: Date},
+//             // La hora a la que finlizo el cambio.
+//             finalizo: { type: Date},
+//             // El usuario mecanico
+//             empleado: {type: ObjectId, ref: 'Empleado'},
+//             // El registro de incidencias en el cambio. 
+
+//             incidencias:[ {
+//                 // El registro de la incidencia debe existir para
+//                 // llevar estadisticas. 
+//                 incidencia: { type: ObjectId, ref: 'Incidencias' },
+//                 observaciones: { type:String },
+//                 fotos: [{ type: String }]
+//             }]
+//         }, 
+
+
+//         // Hora de inicializacion del trabajo. 
+//         inicio: { type: Date },
+//         // Hora de finalizacion del trabajo de la orden. 
+//         finalizacion: { type: Date },
+
+//         incidencias:[ {
+//             incidencia: { type: ObjectId, ref: 'Incidencias' },
+//             observaciones: { type:String },
+//             fotos: [`{ type: String }`]
+//         }]
+
+
+
+
+
+// });
+
+
+// // Esta schema se encarga de almacenar los datos 
+// // de trabajo de la maquina. R
+// var trabajoDeMaquinaSchema = new Schema({
+
+//     // La orden que esta actualmente trabajandose. 
+//     ordenActualTrabajando: orden_DatosDeTrabajoEnMaquina,
+
+//     // La lista de ordenes en espera para empezar a trabajar en la maquina. 
+//     ordenesEnEspera: [orden_DatosDeTrabajoEnMaquina],
+
+//     // El historial de trabajo de la maquina. 
+//     historial: [orden_DatosDeTrabajoEnMaquina],
+// });
+
+
+
+
+
 var maquinaSchema = new Schema({
     nombre: { type: String, required: [true, 'El nombre de la máquina no puede estar vacio. Define un nombre.'] },
     clave: { type: String, required: [true, 'Es importante definir el nombre en clave. Si no lo tiene repite el nombre por favor.'], unique: [true, 'La clave de la maquina no se puede repetir.'] },
@@ -29,15 +97,8 @@ var maquinaSchema = new Schema({
     },
 
 
-    // Estos son los datos de trabajo de la maquina. 
-    //  Por el momento sirven para saber que es lo que esta haceindo
-    // la máquina de transformación. 
-    datosDeTrabajo: {
-        modeloTrabajando: {
-            type: Schema.Types.ObjectId,
-            ref: 'ModeloCompleto'
-        }
-    },
+    // trabajo: trabajoDeMaquinaSchema,
+
 
     numeroDeSerie: { type: String },
 

4b37ab93fad174b336cd76acd1b40c6698ae65ac
Angel <rafa.yael@gmail.com>
2019-01-24 07:55:16 -0600

    Primera intereaccion con el route funcionando.

--- config/routes.js
+++ config/routes.js
@@ -48,7 +48,7 @@ module.exports.ROUTES = {
         route: reportesRoute,
         roles: [],
         subRoutes: {}
-    }
+    },
 
 
 
--- routes/reportes/reportes.js
+++ routes/reportes/reportes.js
@@ -1,20 +1,50 @@
-(var express = require('express');
-    var app = express();
+var express = require('express');
+var app = express();
+var RESP = require('../../utils/respStatus');
 
+
+// <!-- 
+// =====================================
+//  Este route se encarga de generar todos los reportes
+//  que necesitamos.
+// =====================================
+// -->
+
+
+
+// REPORTE DE LASER.
+
+app.get('/laser', (req, res) => {
     // <!-- 
     // =====================================
-    //  Este route se encarga de generar todos los reportes
-    //  que necesitamos.
+    //  OBJETIVO DEL REPORTE. 
     // =====================================
     // -->
 
+    // Mostrar la cantidad de ordenes pendientes
+    // que tienen por laserar el departameno. 
+
+    // <!-- 
+    // =====================================
+    //  Que tenemos que hacer?
+    // =====================================
+    // -->
+
+    // Buscar todas las ordenes que se vayan a laserar.
+    // Filtrar por aquellas que no esten terminadas.
+    // Filtrar las que ya pasaron por laser. 
+    // Mostrar como pendiente de surtir si viene de alamcen
+    // Si viene de produccion mostrar departamentos faltantes. 
+
+    // RETORNAMOS ORDENES. 
+
+    return RESP._200(res, 'Mensaje para interfaz ó null', [
+        { tipo: 'El nombre que recivira el dato', datos: 'El dato u objeto' },
+    ]);
 
 
-    // REPORTE DE LASER.
-
-    app.get('/laser', (req, res) => {
 
 
+});
 
-
-    }); module.exports = app;
+module.exports = app;

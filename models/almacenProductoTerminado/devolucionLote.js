4af3250af64057123c54bb28b41787af81ec7562
Angel <rafa.yael@gmail.com>
2019-01-30 12:45:11 -0600

    Update devolucionLote.js
    Mejoras a validaciones.

--- models/almacenProductoTerminado/devolucionLote.js
+++ models/almacenProductoTerminado/devolucionLote.js
@@ -8,25 +8,33 @@ var devolucionLoteSchema = new Schema({
     cliente: { type: Schema.Types.ObjectId, ref: 'Cliente', required: [true, 'El cliente es necesario'] },
     cantidad: {
         type: Number,
-        min: [1, 'El valor que ingresaste ( {VALUE} ) es menor que el permitido ( {MIN} ).'],
-        max: [() => {
-            // La cantidad de salida no puede superar a la existencia actual. 
-            return this.cantidadEntrada;
-        }, 'El valor que ingresaste ( {VALUE} ) supera el tamano del lote orignal; {MAX}'],
-        validate: {
-            validator: function(v) {
+        validate: [{
 
-                return new Promise((resolve, reject) => {
+                validator: function(v) {
+                    return new Promise((resolve, reject) => {
+                        resolve(1 < v);
+                    });
+                },
+                msg: 'El valor que ingresaste ( {VALUE} ) es menor que el permitido ( 1 ).'
+            },
+            {
+                isAsync: true,
+                validator: function(v, cb) {
+
                     // La cantidad de devolucion junto con la existenca no puede superar
                     // el tamano del lote.
+                    let cantidadEntrada = this.parent().cantidadEntrada;
+                    let existencia = this.parent().existencia - v;
 
-                    if (this.cantidadEntrada < (this.existencia + v)) resolve(false);
+                    let msg = `El valor que ingresaste ( ${ v } ) supera la cantidad original del lote junto con las existencias.
+                    Cantidad original: ${ cantidadEntrada }. Existencia: ${ existencia }`;
 
+                    cb(cantidadEntrada < existencia, msg);
 
-                });
-            },
-            message: me => `${me.value} supera la cantidad original del lote junto con las existencias.`
-        }
+                },
+            }
+
+        ]
     },
     observaciones: { type: String }
 }, { timestamps: true });

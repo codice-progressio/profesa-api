2818ed72e2418a46aba80772f2e8769a7984bb54
Angel <rafa.yael@gmail.com>
2019-01-30 12:26:05 -0600

    Update salidaLote.js
    Validacion de maximo contra existencia funcionando.

--- models/almacenProductoTerminado/salidaLote.js
+++ models/almacenProductoTerminado/salidaLote.js
@@ -11,11 +11,38 @@ var salidaLoteSchema = new Schema({
     cliente: { type: Schema.Types.ObjectId, ref: 'Cliente', required: [true, 'El cliente es necesario'] },
     cantidad: {
         type: Number,
-        min: [1, 'El valor que ingresaste ( {VALUE} ) es menor que el permitido ( {MIN} ).'],
-        max: [() => {
-            // La cantidad de salida no puede superar a la existencia actual. 
-            return this.existencia;
-        }, 'El valor que ingresaste ( {VALUE} ) supera la existencia actual: {MAX}']
+        validate: [{
+                validator: function(v) {
+                    return new Promise((resolve, reject) => {
+                        // El valor minimo no puede ser menor que 1;
+                        resolve(1 < v);
+                    });
+                },
+                msg: 'El valor que ingresaste ( {VALUE} ) es menor que el permitido ( 1 ).'
+            },
+            {
+                isAsync: true,
+                /**
+                 * Esta funcion valida que la cantidad que se va a registrar como salida
+                 * no supera la existencia actual que hay. Es necesario hacer un pequeno hackj
+                 * por que el pre hook save que tenemos para calcular la existencia modifica 
+                 * el atributo antes de antrat aqui y no podemos comparar correctamente. Para
+                 * eso sumanos el valor ingresaso a la existencia y tenemos la existencia 
+                 * actual. 
+                 * 
+                 * @param {number} v El valor que recivimos desde el validador(Que ingreso el usuario.)
+                 * @param {any} cb El callback que genera mongoose y que nos permite agregar el mensaje personalizado.
+                 */
+                validator: function(v, cb) {
+                    let existencia = this.parent().existencia;
+                    // Corregimos por que se aplica el pre donde calcula los totales
+                    // en el pre save. Que es antes de entrar a validar. Por tanto 
+                    // sumamos el valor ingresado para volver a la existencia actual. 
+                    let msjError = `El valor que ingresaste ( ${v} ) es mayor que la existencia ( ${ existencia + v } ) de este lote.`;
+                    cb(existencia > v, msjError)
+                },
+            },
+        ]
     }
 
 }, { timestamps: true });

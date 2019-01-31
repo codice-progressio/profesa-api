5af9b74b8bfa5ea214987f6aa982e003fb14cfc0
Angel <rafa.yael@gmail.com>
2019-01-31 08:00:06 -0600

    Update lote.js
    Delete creado

--- routes/almacenDeProductoTerminado/lote.js
+++ routes/almacenDeProductoTerminado/lote.js
@@ -39,6 +39,44 @@ app.post('/', (req, res) => {
         });
 });
 
+/**
+ * 
+ * Eliminar un lote por su id. 
+ * 
+ */
+
+app.delete('/:idModeloCompleto/:idLote', (req, res) => {
+
+    let idModeloCompleto = req.params.idModeloCompleto;
+    let idLote = req.params.idLote;
+
+    ModeloCompleto.findOne({ _id: idModeloCompleto })
+        .exec()
+        .then(mc => {
+
+            if (!mc) throw new Error('No existe el id del modelo que ingresaste.');
+            if (!mc.lotes.id(idLote)) throw new Error('No existe el id del lote que ingresaste.');
+
+            mc.lotes.pull({ _id: idLote });
+
+            return mc.save();
+        })
+        .then(mcActualizado => {
+            return RESP._200(res, 'Se elimino el lote de manera correcta.', [
+                { tipo: 'modeloCompleto', datos: mcActualizado },
+            ]);
+
+        })
+        .catch(err => {
+            return RESP._500(res, {
+                msj: 'Hubo un error al eliminar el lote.',
+                err: err,
+            });
+        });
+
+});
+
+
 
 
 

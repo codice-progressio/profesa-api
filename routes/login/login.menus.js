69d7d0b4d414541a16aefda7d48be02dfe636cf3
Rafael Ramirez <rafa.yael@gmail.com>
2020-01-16 16:57:51 -0600

    Funkando la cosa loka FIXsss

--- routes/login/login.menus.js
+++ routes/login/login.menus.js
@@ -181,10 +181,17 @@ function almacenes() {
         roles: [CONST.ROLES.ALMACEN_MATERIA_PRIMA]
       },
       {
+        titulo: "Produccion - ES",
+        url: "/almacen/produccion/entradasYSalidas",
+        roles: [CONST.ROLES.ALMACEN_MATERIA_PRIMA_ENTRADA_Y_SALIDA]
+      },
+      {
         titulo: "Requisiciones",
         url: "/almacen/requisiciones",
         roles: [CONST.ROLES.ALMACEN_REQUISICION]
       }
+
+     
     ]
   }
   return menu

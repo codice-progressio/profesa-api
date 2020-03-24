fb424363381c6c7f6a4c625fdf5d969f4136b689
Rafael Ramirez <rafa.yael@gmail.com>
2020-03-24 12:45:25 -0600

    CRUD eliminado

--- routes/gestionModelos/modeloCompleto.js
+++ routes/gestionModelos/modeloCompleto.js
@@ -8,26 +8,158 @@ var app = express()
 const mongoose = require("mongoose")
 const ObjectId = mongoose.Types.ObjectId
 
-var CRUD = require("../CRUD")
-CRUD.app = app
-CRUD.modelo = ModeloCompleto
-CRUD.nombreDeObjetoSingular = "modeloCompleto"
-CRUD.nombreDeObjetoPlural = "modelosCompletos"
-CRUD.campoSortDefault = "nombreCompleto"
-CRUD.camposActualizables = {
-  medias: null,
-  laserAlmacen: null,
-  versionModelo: null,
-  familiaDeProcesos: null,
-  procesosEspeciales: null,
-  nombreCompleto: null,
-  porcentajeDeMerma: null,
-  espesor: null
+const erro = (res, err, msj) => {
+  return RESP._500(res, {
+    msj: msj,
+    err: err
+  })
 }
 
-CRUD.camposDeBusqueda = ["nombreCompleto"]
+app.post("/", (req, res) => {
+  return new ModeloCompleto(req.body)
+    .save()
+    .then(modeloCompleto => {
+      return RESP._200(res, "Se guardo el modeloCompleto", [
+        { tipo: "modeloCompleto", datos: modeloCompleto }
+      ])
+    })
+    .catch(err => erro(res, err, "Hubo un error guardo el sku"))
+})
 
-CRUD.crud()
+app.get("/", async (req, res) => {
+  const desde = Number(req.query.desde || 0)
+  const limite = Number(req.query.limite || 30)
+  const sort = Number(req.query.sort || 1)
+  const campo = String(req.query.campo || "nombreCompleto")
+
+  const total = await ModeloCompleto.countDocuments().exec()
+
+  ModeloCompleto.find()
+    .sort({ [campo]: sort })
+    .limit(limite)
+    .skip(desde)
+    .exec()
+    .then(modelosCompletos => {
+      return RESP._200(res, null, [
+        { tipo: "modelosCompletos", datos: modelosCompletos },
+        { tipo: "total", datos: total }
+      ])
+    })
+    .catch(err => erro(res, err, "Hubo un error buscando los sku"))
+})
+
+app.get("/buscar/id/:id", (req, res) => {
+  ModeloCompleto.findById(req.params.id)
+    .exec()
+    .then(modeloCompleto => {
+      if (!modeloCompleto) throw "No existe el id"
+
+      return RESP._200(res, null, [
+        { tipo: "modeloCompleto", datos: modeloCompleto }
+      ])
+    })
+    .catch(err => erro(res, err, "Hubo un error buscando el sku por su id"))
+})
+
+app.get("/buscar/termino/:termino", async (req, res) => {
+  const desde = Number(req.query.desde || 0)
+  const limite = Number(req.query.limite || 30)
+  const sort = Number(req.query.sort || 1)
+  const campo = String(req.query.campo || "nombreCompleto")
+  const termino = String(
+    req.params.termino.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
+  )
+  const b = campo => ({
+    [campo]: { $regex: termino, $options: "i" }
+  })
+
+  const $match = {
+    $or: []
+  }
+
+  ;["nombreCompleto"].forEach(x => $match.$or.push(b(x)))
+
+  const total = await ModeloCompleto.aggregate([
+    { $match },
+    { $count: "total" }
+  ]).exec()
+
+  ModeloCompleto.aggregate([
+    { $match },
+
+    //Fin de populacion
+
+    { $sort: { [campo]: sort } },
+    //Desde aqui limitamos unicamente lo que queremos ver
+    { $limit: desde + limite },
+    { $skip: desde },
+    { $sort: { [campo]: sort } },
+    {
+      $project: {
+        nombreCompleto: "$nombreCompleto",
+        existencia: "$existencia",
+        stockMaximo: "$stockMaximo",
+        stockMinimo: "$stockMinimo"
+      }
+    }
+  ])
+    .exec()
+    .then(modelosCompletos => {
+      //Si no hay resultados no se crea la propiedad
+      // y mas adelante nos da error.
+      if (!total.length) total.push({ total: 0 })
+
+      return RESP._200(res, null, [
+        { tipo: "modelosCompletos", datos: modelosCompletos },
+        { tipo: "total", datos: total.pop().total }
+      ])
+    })
+    .catch(err =>
+      erro(res, err, "Hubo un error buscando los sku por el termino " + termino)
+    )
+})
+
+app.delete("/:id", (req, res) => {
+  ModeloCompleto.findById(req.params.id)
+    .exec()
+    .then(modeloCompleto => {
+      if (!modeloCompleto) throw "No existe el sku"
+
+      return modeloCompleto.remove()
+    })
+    .then(modeloCompleto => {
+      return RESP._200(res, "Se elimino de manera correcta", [
+        { tipo: "modeloCompleto", datos: modeloCompleto }
+      ])
+    })
+    .catch(err => erro(res, err, "Hubo un error eliminando el sku"))
+})
+
+app.put("/", (req, res) => {
+  ModeloCompleto.findById(req.body._id)
+    .exec()
+    .then(modeloCompleto => {
+      if (!modeloCompleto) throw "No existe el sku"
+      ;[
+        "medias",
+        "laserAlmacen",
+        "versionModelo",
+        "familiaDeProcesos",
+        "procesosEspeciales",
+        "nombreCompleto",
+        "porcentajeDeMerma",
+        "espesor"
+      ].forEach(x => (modeloCompleto[x] = req.body[x]))
+
+      return modeloCompleto.save()
+    })
+    .then(sku => {
+      return RESP._200(res, "Se modifico correctamente", [
+        { tipo: "modeloCompleto", datos: sku }
+      ])
+    })
+    .catch(err => erro(res, err, "Hubo un error actualizando el sku"))
+})
 
 app.get("/transito/:id", (req, res) => {
   let id = req.params.id

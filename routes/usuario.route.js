7f8d0d5efd3432dcd2a9108b72c9959d0fa588dc
Rafael Ramirez <rafa.yael@gmail.com>
2020-04-19 07:26:37 -0500

    Operaciones mejoradas para usuario

--- routes/usuario.route.js
+++ routes/usuario.route.js
@@ -6,7 +6,6 @@ var Usuario = require("../models/usuario")
 var CONST = require("../utils/constantes")
 var RESP = require("../utils/respStatus")
 
-
 var app = express()
 
 var guard = require("express-jwt-permissions")()
@@ -22,7 +21,7 @@ app.get(
     var desde = req.query.desde || 0
     desde = Number(desde)
 
-    Usuario.find({}, "nombre email img role google")
+    Usuario.find({}, "nombre email img permissions google")
       //Se salta los primeros "desde" registros y carga los siguientes.
       .skip(desde)
       // Liminta la cantidad de registros que se van a mostrar.
@@ -55,47 +54,63 @@ app.get(
 // ============================================
 // Actualizar usuario
 // ============================================
-app.put("/:id", guard.check(permisos.$("usuario:modificar")), (req, res) => {
-  var id = req.params.id
-  var body = req.body
+app.put(
+  "/",
+  guard.check(permisos.$("administrador:usuario:modificar")),
+  (req, res) => {
+    var id = req.body._id
+    var body = req.body
 
-  //Eliminamos todos los roles que no existen.
-  body.role = body.role.filter((rol) => CONST.ROLES.ARRAY.includes(rol))
-  Usuario.findById(id)
-    .exec()
-    .then((u) => {
-      if (!u) {
-        return RESP._400(res, {
-          msj: "El usuario no existe. ",
-          err: "El id que ingresaste no esta registrado con ningún usuario.",
-        })
-      }
+    //Eliminamos todos los roles que no existen.
+    body.permissions = body.permissions.filter(permiso =>
+      permisos.lista.includes(permiso)
+    )
+    Usuario.findById(id)
+      .exec()
+      .then(u => {
+        if (!u) throw "No exise el id"
 
-      u.nombre = body.nombre
-      u.email = body.email
+        u.nombre = body.nombre
+        u.email = body.email
+        
+        while (u.permissions.length > 0)
+        {
+          u.permissions.pop()
+        }
+        
+        req.body.permissions.forEach(x => u.permissions.push(x))
+        // Si se agrega un password si se modifica MIENTRAS NO SEA SUPER ADMIN.
+        if (body.password) {
+          u.password = bcrypt.hashSync(body.password, 10)
+        }
+        if (u._id.toString() === req.parametros.super.id) {
+          //Es super usuario, debe incluir SUPER_ADMIN y login
 
-      // Si se agrega un password si se modifica MIENTRAS NO SEA SUPER ADMIN.
-      if (body.password) {
-        u.password = bcrypt.hashSync(body.password, 10)
-      }
+          if (!u.permissions.includes("SUPER_ADMIN"))
+            u.permissions.push(permisos.$("SUPER_ADMIN"))
 
-      return u.save()
-    })
-    .then((uG) => {
-      uG.password = ":D"
-      return RESP._200(
-        res,
-        `Se actualizo el usuario ${uG.nombre} correctamente.`,
-        [{ tipo: "usuario", datos: uG }]
-      )
-    })
-    .catch((err) => {
-      return RESP._500(res, {
-        msj: "Hubo un error actualizando el usuario.",
-        err: err,
+          if (!u.permissions.includes("login"))
+            u.permissions.push(permisos.$("login"))
+        }
+
+        return u.save()
       })
-    })
-})
+      .then(uG => {
+        uG.password = ":D"
+        return RESP._200(
+          res,
+          `Se actualizo el usuario ${uG.nombre} correctamente.`,
+          [{ tipo: "usuario", datos: uG }]
+        )
+      })
+      .catch(err => {
+        return RESP._500(res, {
+          msj: "Hubo un error actualizando el usuario.",
+          err: err,
+        })
+      })
+  }
+)
 
 // ============================================
 // Crear un nuevo usuario.
@@ -119,7 +134,7 @@ app.post(
 
     usuario
       .save()
-      .then((uGuardado) => {
+      .then(uGuardado => {
         console.log(" Esta aqua")
 
         return RESP._200(
@@ -131,7 +146,7 @@ app.post(
           ]
         )
       })
-      .catch((err) => {
+      .catch(err => {
         console.log(err)
 
         return RESP._500(res, {
@@ -139,8 +154,6 @@ app.post(
           err: err,
         })
       })
-
-
   }
 )
 
@@ -189,28 +202,75 @@ app.delete(
   }
 )
 
-app.put(
-  "/roles",
+app.get(
+  "/buscar/termino/:termino",
+  guard.check(permisos.$("administrador:usuario:leer:termino")),
+  async (req, res) => {
+    const desde = Number(req.query.desde || 0)
+    const limite = Number(req.query.limite || 30)
+    const sort = Number(req.query.sort || 1)
+    const campo = String(req.query.campo || "nombre")
+    const termino = String(
+      req.params.termino.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
+    )
+    const b = campo => ({
+      [campo]: { $regex: termino, $options: "i" },
+    })
 
-  guard.check(permisos.$("administrador:usuario:roles")),
+    const $match = {
+      $or: [],
+    }
 
-  (req, res) => {
-    Usuario.findById(req.body._id)
+    ;["nombre"].forEach(x => $match.$or.push(b(x)))
+
+    const total = await Usuario.aggregate([
+      { $match },
+      { $count: "total" },
+    ]).exec()
+
+    Usuario.aggregate([
+      { $match },
+
+      //Fin de populacion
+
+      { $sort: { [campo]: sort } },
+      //Desde aqui limitamos unicamente lo que queremos ver
+      { $limit: desde + limite },
+      { $skip: desde },
+      { $sort: { [campo]: sort } },
+    ])
       .exec()
-      .then((usuario) => {
-        if (!usuario) throw "No existe el usuario"
+      .then(usuarios => {
+        //Si no hay resultados no se crea la propiedad
+        // y mas adelante nos da error.
+        if (!total.length) total.push({ total: 0 })
 
-        while (usuario.permissions > 0) usuario.permissions.pop()
-
-        req.body.forEach((x) => usuario.permissions.push(x))
-
-        return usuario.save()
+        return RESP._200(res, null, [
+          { tipo: "usuarios", datos: usuarios },
+          { tipo: "total", datos: total.pop().total },
+        ])
       })
-      .then((usuario) => {
-        return RESP._200(res, "Se modificaron los roles del usuario", [{}])
-      })
-      .catch((err) => next(err))
+      .catch(err =>
+        erro(
+          res,
+          err,
+          "Hubo un error buscando los usuarios por el termino " + termino
+        )
+      )
   }
 )
 
+app.get(
+  "/buscar/id/:id",
+  guard.check(permisos.$("administrador:usuario:leer:id")),
+  (req, res) => {
+    Usuario.findById(req.params.id)
+      .exec()
+      .then(usuario => {
+        if (!usuario) throw "No existe el usuario"
+        return RESP._200(res, null, [{ tipo: "usuario", datos: usuario }])
+      })
+      .catch(err => erro(res, err, "Hubo un error buscando el usuario por id"))
+  }
+)
 module.exports = app

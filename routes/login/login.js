2bff9bdae7df01d38a333e329551a29c5e4709f2
Rafael Ramirez <rafa.yael@gmail.com>
2020-04-19 07:26:16 -0500

    FIX Se renueva token

--- routes/login/login.js
+++ routes/login/login.js
@@ -17,20 +17,41 @@ const obtenerMenu = require("./login.menus")
 var guard = require("express-jwt-permissions")()
 var permisos = require("../../config/permisos.config")
 
-// ============================================
-// Autenticación de google.
-// ============================================
+function crearToken(usuario) {
+  return jwt.sign({ ...usuario }, SEED, {
+    //Una hora 3600
+    expiresIn: 3600*2,
+  })
+}
 
-app.get("/renuevatoken", (req, res) => {
-  throw "Pendiende de implementacin"
+app.post("/renuevatoken", (req, res) => {
+  Usuario.findById(req.user._id)
+    .select('+passsword')
+    .lean()
+    .exec()
+    .then(usuario => {
+      if (!usuario) throw "No se renovo la sesion"
+
+      let token = crearToken(usuario)
+      return RESP._200(res, "Se renovo la sesion", [
+        { tipo: "token", datos: token },
+      ])
+    })
+    .catch(err => {
+      return RESP._500(res, {
+        msj: "Hubo un error en el login.",
+        err: err,
+      })
+    })
 })
 
 app.post("/", (req, res) => {
   var body = req.body
   Usuario.findOne({ email: body.email })
+    .select('+password')
     .lean()
     .exec()
-    .then((usuarioDB) => {
+    .then(usuarioDB => {
       if (!usuarioDB) throw "Credenciales incorrectas"
 
       if (!body.password) throw "El password no debe estar vacio."
@@ -40,9 +61,7 @@ app.post("/", (req, res) => {
 
       // crear un token!
       delete usuarioDB.password
-      var token = jwt.sign({ ...usuarioDB }, SEED, {
-        expiresIn: 14400,
-      })
+      var token = crearToken(usuarioDB)
 
       return RESP._200(res, `Bienvenido ${usuarioDB.nombre}`, [
         { tipo: "usuario", datos: usuarioDB },
@@ -52,7 +71,7 @@ app.post("/", (req, res) => {
         { tipo: "apiVersion", datos: pjson.version },
       ])
     })
-    .catch((err) => {
+    .catch(err => {
       return RESP._500(res, {
         msj: "Hubo un error en el login.",
         err: err,
@@ -61,10 +80,8 @@ app.post("/", (req, res) => {
 })
 
 // Retorna todos los roles que hay en la api.
-app.get("/roles", guard.check(permisos.$("SUPER_ADMIN")), (req, res) => {
-  let roles = CONST.ROLES
-
-  RESP._200(res, null, [{ tipo: "roles", datos: roles }])
+app.get("/permisos", guard.check(permisos.$("SUPER_ADMIN")), (req, res) => {
+  RESP._200(res, null, [{ tipo: "permisos", datos: permisos.lista }])
 })
 
 module.exports = app

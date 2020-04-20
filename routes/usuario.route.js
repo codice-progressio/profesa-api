1c4e88efd4ca5613ccd967612a84ddf39d54a55a
Rafael Ramirez <rafa.yael@gmail.com>
2020-04-20 15:45:12 -0500

    MEjoras

--- routes/usuario.route.js
+++ routes/usuario.route.js
@@ -18,35 +18,20 @@ app.get(
   "/",
   guard.check(permisos.$("administrador:usuario:leer")),
   (req, res, next) => {
-    var desde = req.query.desde || 0
-    desde = Number(desde)
+    const desde = Number(req.query.desde || 0)
+    const limite = Number(req.query.limite || 30)
+    const sort = Number(req.query.sort || 1)
+    const campo = String(req.query.campo || "nombre")
 
-    Usuario.find({}, "nombre email img permissions google")
+    Usuario.find()
       //Se salta los primeros "desde" registros y carga los siguientes.
       .skip(desde)
       // Liminta la cantidad de registros que se van a mostrar.
-      .limit(5)
-      .exec((err, usuarios) => {
-        if (err) {
-          return res.status(500).json({
-            ok: true,
-            mensaje: "Error cargando usuarios.",
-            errors: err,
-          })
-        }
-
-        // Esta función cuenta la cantidad de registros que existen.
-        // Estos datos se pasan en el json para llevar la paginación.
-        Usuario.count({}, (err, conteo) => {
-          // Si no sucede ningún error.
-          res.status(200).json({
-            ok: true,
-            usuarios: usuarios,
-            total: conteo - 1,
-          })
-        })
-
-        console.log(colores.info("GET") + " Petición correcta: Usuarios")
+      .limit(limite)
+      .sort({ [campo]: sort })
+      .exec()
+      .then(usuarios => {
+        return RESP._200(res, null, [{ tipo: "usuarios", datos: usuarios }])
       })
   }
 )
@@ -60,7 +45,6 @@ app.put(
   (req, res) => {
     var id = req.body._id
     var body = req.body
-
     //Eliminamos todos los roles que no existen.
     body.permissions = body.permissions.filter(permiso =>
       permisos.lista.includes(permiso)
@@ -72,15 +56,14 @@ app.put(
 
         u.nombre = body.nombre
         u.email = body.email
-        
-        while (u.permissions.length > 0)
-        {
+
+        while (u.permissions.length > 0) {
           u.permissions.pop()
         }
-        
+
         req.body.permissions.forEach(x => u.permissions.push(x))
-        // Si se agrega un password si se modifica MIENTRAS NO SEA SUPER ADMIN.
         if (body.password) {
+          // Si se agrega un password si se modifica MIENTRAS NO SEA SUPER ADMIN.
           u.password = bcrypt.hashSync(body.password, 10)
         }
         if (u._id.toString() === req.parametros.super.id) {
@@ -119,41 +102,17 @@ app.post(
   "/",
   guard.check(permisos.$("administrador:usuario:crear")),
   (req, res) => {
-    var body = req.body
-
-    var usuario = new Usuario({
-      nombre: body.nombre,
-      email: body.email,
-      password: bcrypt.hashSync(body.password ? body.password : "", 10),
-      img: body.img,
-      role: body.role,
-      idTrabajador: body.idTrabajador,
-    })
-
-    console.log(usuario)
-
+    var usuario = new Usuario(req.body)
     usuario
       .save()
       .then(uGuardado => {
-        console.log(" Esta aqua")
-
         return RESP._200(
           res,
           `Usuario '${uGuardado.nombre}' creado con éxito.`,
-          [
-            { tipo: "usuario", datos: uGuardado },
-            { tipo: "usuarioToken", datos: req.usuario },
-          ]
+          [{ tipo: "usuario", datos: uGuardado }]
         )
       })
-      .catch(err => {
-        console.log(err)
-
-        return RESP._500(res, {
-          msj: "Hubo un error guardando el usuario.",
-          err: err,
-        })
-      })
+      .catch(err => erro(res, err, "Hubo un error creando el usuario"))
   }
 )
 
@@ -165,40 +124,23 @@ app.delete(
   "/:id",
   guard.check(permisos.$("administrador:usuario:eliminar")),
   (req, res) => {
-    var id = req.params.id
+    Usuario.findById(req.params.id)
+      .exec()
+      .then(u => {
+        if (!u) throw "No existe el id"
+        // No puede ser el super_admin
 
-    Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
-      if (err) {
-        var msj = "Error al borrar usuario"
-        console.log(
-          colores.danger("Error DELETE - Usuario") + `${msj} =>` + err
-        )
-        return res.status(500).json({
-          ok: false,
-          mensaje: `${msj}`,
-          errors: err,
-        })
-      }
+        if (req.parametros.super.id === u._id.toObject())
+          throw "Imposible eliminar este usuario. "
 
-      if (!usuarioBorrado) {
-        var msj2 = "No existe un usuario con ese id."
-
-        console.log(
-          colores.danger("Error DELETE - Usuario") + `${msj2} =>` + err
-        )
-        return res.status(400).json({
-          ok: false,
-          mensaje: msj2,
-          errors: { message: msj2 },
-        })
-      }
-
-      console.log(colores.info("DELETE") + " Petición correcta: Usuarios")
-      res.status(200).json({
-        ok: true,
-        usuario: usuarioBorrado,
+        return usuario.remove()
       })
-    })
+      .then(ur => {
+        return RESP._200(res, "Se elimino el usuario", [
+          { tipo: "ur", datos: ur },
+        ])
+      })
+      .catch(err => erro(res, err, "Hubo un error eliminado el usuario"))
   }
 )
 
@@ -254,7 +196,7 @@ app.get(
         erro(
           res,
           err,
-          "Hubo un error buscando los usuarios por el termino " + termino
+          `Hubo un error buscando usuarios por el termino "${termino}"`
         )
       )
   }

a88a1a14ce6b2d2d479120af42def5ba716172e9
Rafael Ramirez <rafa.yael@gmail.com>
2021-01-30 10:49:24 -0600

    Si no se inicia la BD no arranca la app.

--- app.js
+++ app.js
@@ -38,79 +38,79 @@ mongoose.connection.openUri(process.env.URI, (err, res) => {
   }
   // Mensaje de conexion exitosa a la BD
   console.log("[ INFO ] Conectado a la BD")
-})
 
-app.use((req, res, next) => {
-  if (process.env.PROUDCCION === "true") {
-    console.log(
-      `${new Date()}|` +
-        colores.success("PETICION RECIBIDA") +
-        colores.danger(req.method) +
-        colores.info(req.originalUrl)
-    )
-  }
-  next()
-})
-
-_ROUTES(app)
-
-// Llamamos a los errores.
-app.use(function (req, res) {
-  return res.status(404).send("No existe la pagina")
-})
-
-app.use(function (err, req, res, next) {
-  console.log(`err`, err)
-  //Errores de permisos
-  const errores = [
-    //Cuando el token no trae un usuario
-    "user_object_not_found",
-    //No autorizado
-    "permission_denied",
-  ]
-
-  if (errores.includes(err.code)) {
-    return res
-      .status(403)
-      .send(
-        `No tienes permisos para acceder a este contenido: '${req.permisoSolicitado}'`
+  app.use((req, res, next) => {
+    if (process.env.PROUDCCION === "false") {
+      console.log(
+        `${new Date()}|` +
+          colores.success("PETICION RECIBIDA") +
+          colores.danger(req.method) +
+          colores.info(req.originalUrl)
       )
+    }
+    next()
+  })
+
+  _ROUTES(app)
+
+  // Llamamos a los errores.
+  app.use(function (req, res) {
+    return res.status(404).send("No existe la pagina")
+  })
+
+  app.use(function (err, req, res, next) {
+    console.log(`err`, err)
+    //Errores de permisos
+    const errores = [
+      //Cuando el token no trae un usuario
+      "user_object_not_found",
+      //No autorizado
+      "permission_denied",
+    ]
+
+    if (errores.includes(err.code)) {
+      return res
+        .status(403)
+        .send(
+          `No tienes permisos para acceder a este contenido: '${req.permisoSolicitado}'`
+        )
+    }
+
+    if (err.code === "invalid_token") {
+      return res.status(401).send("Token invalido. Inicia sesion de nuevo")
+    }
+
+    if (err.code === "credentials_required") {
+      return res.status(401).send("Es necesario loguearte")
+    }
+
+    if (err.errors) {
+      return res.status(500).send(err.message)
+    }
+
+    return res.status(500).send(err)
+  })
+
+  const msjServidor = () => {
+    console.log(`Servidor iniciado en el puerto: ${process.env.PORT}`)
   }
 
-  if (err.code === "invalid_token") {
-    return res.status(401).send("Token invalido. Inicia sesion de nuevo")
+  if (process.env.PRODUCCION === "true") {
+    console.log("[ INFO ] Modo produccion")
+    app.listen(process.env.PORT, msjServidor)
+  } else {
+    https
+      .createServer(
+        {
+          key: fs.readFileSync(
+            "f:/proyectos/geracion-de-certificados/cert/desarrollo.key"
+          ),
+          cert: fs.readFileSync(
+            "f:/proyectos/geracion-de-certificados/cert/desarrollo.crt"
+          ),
+        },
+        app
+      )
+      .listen(process.env.PORT, msjServidor)
   }
-
-  if (err.code === "credentials_required") {
-    return res.status(401).send("Es necesario loguearte")
-  }
-
-  if (err.errors) {
-    return res.status(500).send(err.message)
-  }
-
-  return res.status(500).send(err)
 })
-
-const msjServidor = () => {
-  console.log(`Servidor iniciado en el puerto: ${process.env.PORT}`)
-}
-
-if (process.env.PRODUCCION === "true") {
-  console.log("[ INFO ] Modo produccion")
-  app.listen(process.env.PORT, msjServidor)
-} else {
-  https
-    .createServer(
-      {
-        key: fs.readFileSync(
-          "f:/proyectos/geracion-de-certificados/cert/desarrollo.key"
-        ),
-        cert: fs.readFileSync(
-          "f:/proyectos/geracion-de-certificados/cert/desarrollo.crt"
-        ),
-      },
-      app
-    )
-    .listen(process.env.PORT, msjServidor)
-}

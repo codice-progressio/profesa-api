bdc6575e890a7a151f27ba5655a39160ba2e0da2
Rafael Ramirez <rafa.yael@gmail.com>
2021-02-27 11:57:37 -0600

    Mega comentarios

--- config/routes.js
+++ config/routes.js
@@ -52,83 +52,83 @@ const $ = require("@codice-progressio/easy-permissions").$
 
 //Aseguramos todo menos el login y paremetros. Internamente paraemtros
 // se asegura. Tambien crea el req.user
-const rutasAExcluir = [
-  "/parametros",
-  "/parametros/super-admin/crear",
-  "/login",
-  "/img/usuarios/xxx",
-]
-app.use(
-  jwt({ secret: process.env.SEED, algorithms: ["HS256"] }).unless({
-    path: rutasAExcluir,
-  })
-)
+// const rutasAExcluir = [
+//   "/parametros",
+//   "/parametros/super-admin/crear",
+//   "/login",
+//   "/img/usuarios/xxx",
+// ]
+// app.use(
+//   jwt({ secret: process.env.SEED, algorithms: ["HS256"] }).unless({
+//     path: rutasAExcluir,
+//   })
+// )
 
-//Excluimos rutas
-app.use(
-  guard.check("login").unless({
-    path: rutasAExcluir,
-  })
-)
+// //Excluimos rutas
+// app.use(
+//   guard.check("login").unless({
+//     path: rutasAExcluir,
+//   })
+// )
 
 // app.use("/img", imagenesRoutes)
 //Este va primero por que se usan permisos especiales internamente
 app.use("/parametros", Parametros)
 
-//Cargamos todos los parametros en cada peticion para tener disponible
-//la informacion en req.parametros
-const ParametrosModel = require("../models/defautls/parametros.model")
-app.use((req, res, next) => {
-  ParametrosModel.findOne()
-    .exec()
-    .then(parametros => {
-      if (!parametros)
-        throw "No has definido el documento que contiene los parametros. Es necesario que los definas para poder continuar."
+// //Cargamos todos los parametros en cada peticion para tener disponible
+// //la informacion en req.parametros
+// const ParametrosModel = require("../models/defautls/parametros.model")
+// app.use((req, res, next) => {
+//   ParametrosModel.findOne()
+//     .exec()
+//     .then(parametros => {
+//       if (!parametros)
+//         throw "No has definido el documento que contiene los parametros. Es necesario que los definas para poder continuar."
 
-      req["parametros"] = parametros
-      next()
-    })
-    .catch(err => next(err))
-})
-app.use("/login", loginRoutes)
+//       req["parametros"] = parametros
+//       next()
+//     })
+//     .catch(err => next(err))
+// })
+// app.use("/login", loginRoutes)
 
 //Para usar esta parte debe tener permisos de login
 
 // app.use("/changelogs", Changelogs)
 
-app.use("/programacionTransformacion", ProgramacionTransformacion)
-app.use(
-  "/reportePersonalizadoAlmacenProduccion",
-  ReportePersonalizadoAlmacenProduccion
-)
+// app.use("/programacionTransformacion", ProgramacionTransformacion)
+// app.use(
+//   "/reportePersonalizadoAlmacenProduccion",
+//   ReportePersonalizadoAlmacenProduccion
+// )
 
-// app.use("/empleado", EmpleadoRoute)
-// app.use("/puesto", PuestoRoute)
-// app.use("/area", AreaRoute)
-// app.use("/curso", CursoRoute)
-// app.use("/requisicion", RequisicionRoute)
-// app.use("/divisa", DivisaRoute)
-app.use("/proveedor", proveedorRoute)
-// app.use("/articulo", articuloRoute)
-// app.use("/almacenDescripcion", almacenDescripcionRoute)
-// app.use("/almacenDeProductoTerminado", almacenDeProductoTerminadoRoute)
-// app.use("/reportes", reportesRoute)
+// // app.use("/empleado", EmpleadoRoute)
+// // app.use("/puesto", PuestoRoute)
+// // app.use("/area", AreaRoute)
+// // app.use("/curso", CursoRoute)
+// // app.use("/requisicion", RequisicionRoute)
+// // app.use("/divisa", DivisaRoute)
+// app.use("/proveedor", proveedorRoute)
+// // app.use("/articulo", articuloRoute)
+// // app.use("/almacenDescripcion", almacenDescripcionRoute)
+// // app.use("/almacenDeProductoTerminado", almacenDeProductoTerminadoRoute)
+// // app.use("/reportes", reportesRoute)
 
-// //Gestion de folios
-// app.use("/folios", folioNewRoutes)
-// app.use("/orden", ordenRoutes)
-// //----------------------------
-app.use("/usuario", usuarioRoutes)
+// // //Gestion de folios
+// // app.use("/folios", folioNewRoutes)
+// // app.use("/orden", ordenRoutes)
+// // //----------------------------
+// app.use("/usuario", usuarioRoutes)
 // app.use("/upload", uploadRoutes)
 
-app.use("/sku", skuRoutes)
-// app.use("/cliente", clienteRoutes)
-// app.use("/departamento", departamentoRoutes)
-// app.use("/proceso", procesoRoutes)
-// app.use("/familiaDeProcesos", familiaDeProcesosRoutes)
-// app.use("/maquina", maquinaRoutes)
-app.use("/pedido", pedidoRoute)
-app.use("/estadisticas", estadisticasRoute)
-app.use("/ruta-de-entrega", rutaDeEntregaRoute)
+// app.use("/sku", skuRoutes)
+// // app.use("/cliente", clienteRoutes)
+// // app.use("/departamento", departamentoRoutes)
+// // app.use("/proceso", procesoRoutes)
+// // app.use("/familiaDeProcesos", familiaDeProcesosRoutes)
+// // app.use("/maquina", maquinaRoutes)
+// app.use("/pedido", pedidoRoute)
+// app.use("/estadisticas", estadisticasRoute)
+// app.use("/ruta-de-entrega", rutaDeEntregaRoute)
 module.exports = app
 //

30ec2925688a4b27cde6491f13c4b7102a051de6
Rafael Ramirez <rafa.yael@gmail.com>
2021-02-27 12:59:01 -0600

    menos opciones

--- app.js
+++ app.js
@@ -39,10 +39,9 @@ app.use((req, res, next) => {
 
 var corsOptions = {
   origin: process.env.ORIGIN,
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  credentials: true,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
 }
+
 console.log(corsOptions)
 app.use(cors(corsOptions))
 

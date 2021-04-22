e2c9109beffed44b4e342ac19a1ebb8a80ba05b0
Rafael Ramirez <rafa.yael@gmail.com>
2021-04-22 14:04:55 -0500

    Codigo mas entendible

--- routes/login/login.route.js
+++ routes/login/login.route.js
@@ -19,11 +19,14 @@ function crearToken(usuario) {
     apiVersion: pjson.version,
   }
 
+  //Una hora 3600
+  const hora = 3600
+
   let data = JSON.stringify({ ...objeto })
   data = JSON.parse(data.normalize("NFD").replace(/[\u0300-\u036f]/g, ""))
   return jwt.sign(data, SEED, {
-    //Una hora 3600
-    expiresIn: 3600 * 2,
+    // expiresIn: 3600 * 2,
+    expiresIn: hora * 2,
   })
 }
 
